9c2c94a2747436e9e60a8582e6b7c0bd2a33124e
Kris Budde <kris@budd.ee>
2023-11-30 17:11:54 +0100

    Update deploy.sh with a warning

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ export AVP_TYPE=sops
 
 for app in "$base"/*; do
   echo "argocd-vault-plugin generate \"$app\" | kapp deploy  -c -a \"${app#"$base/"}\" -f -"
+  #! Warning errors not handled and workload is deleted from the cluster :)
   argocd-vault-plugin generate "$app" | kapp deploy -y -c -a "${app#"$base/"}" -f - --apply-default-update-strategy fallback-on-replace
 done
